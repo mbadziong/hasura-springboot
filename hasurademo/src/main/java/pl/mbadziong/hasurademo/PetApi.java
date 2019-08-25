98261caccf8272325c709a224f5b6aab6efd90cf
mbadziong <traceur775@gmail.com>
2019-08-25 09:26:54 +0200

    added missing POST api, fixed mutation syntax

--- hasurademo/src/main/java/pl/mbadziong/hasurademo/PetApi.java
+++ hasurademo/src/main/java/pl/mbadziong/hasurademo/PetApi.java
@@ -2,6 +2,7 @@ package pl.mbadziong.hasurademo;
 
 import graphql.pl.mbadziong.PetQueries;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +16,11 @@ public class PetApi {
     public List<PetQueries.PetQuery.Result.pet> getAllPets() {
         var result = PetQueries.PetQuery.builder().build().request(ENDPOINT).post();
         return result.getPet();
+    }
+
+    @PostMapping("/")
+    public PetQueries.PetMutation.Result.insert_pet addPet(String name) {
+        var result = PetQueries.PetMutation.create(name).request(ENDPOINT).post();
+        return result.getInsert_pet();
     }
 }
